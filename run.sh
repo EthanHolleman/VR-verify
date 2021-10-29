442155074edf5bede895300a7421bef0770be953
Your Name <etholleman@ucdavis.edu>
2021-10-28 21:56:42 -0700

    increase allowed jobs to equal number inserts

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 mkdir -p logs
 source ~/anaconda3/etc/profile.d/conda.sh
-snakemake -j 10 --cluster-config cluster.yml \
+snakemake -j 31 --cluster-config cluster.yml \
 --cluster "sbatch -p {cluster.partition} -t {cluster.time} -N {cluster.nodes} \
 -n {cluster.cpus} --mem {cluster.mem} -J {cluster.name} -o {cluster.output} \
 -e {cluster.output} --mail-type ALL --mail-user {cluster.email}" \
