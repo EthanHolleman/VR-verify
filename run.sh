5c456962d4c43e4392293d87dcd1081031d112e2
Your Name <etholleman@ucdavis.edu>
2021-10-28 21:13:55 -0700

    remove snakemake activation

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 mkdir -p logs
 source ~/anaconda3/etc/profile.d/conda.sh
-conda activate snakemake
 snakemake -j 10 --cluster-config cluster.yml \
 --cluster "sbatch -p {cluster.partition} -t {cluster.time} -N {cluster.nodes} \
 -n {cluster.cpus} --mem {cluster.mem} -J {cluster.name} -o {cluster.output} \
 -e {cluster.output} --mail-type ALL --mail-user {cluster.email}" \
---conda-frontend=mamba --latency-wait 60 \
---use-conda --rerun-incomplete
+--conda-frontend=mamba --latency-wait 45 \
+--use-conda
