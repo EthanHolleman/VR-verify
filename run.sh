870ad5b2bf0b9b95047a343e46a7067851ab6b94
Your Name <etholleman@ucdavis.edu>
2021-11-29 12:26:15 -0800

    increase lat wait

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ snakemake -j 31 --cluster-config cluster.yml \
 --cluster "sbatch -p {cluster.partition} -t {cluster.time} -N {cluster.nodes} \
 -n {cluster.cpus} --mem {cluster.mem} -J {cluster.name} -o {cluster.output} \
 -e {cluster.output} --mail-type ALL --mail-user {cluster.email}" \
---conda-frontend=mamba --latency-wait 45 \
+--conda-frontend=mamba --latency-wait 60 \
 --use-conda
